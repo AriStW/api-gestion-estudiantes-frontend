122376b9b46a7c838cba4f610db4493231135da1
AriStWeb <sole21008@gmail.com>
2025-10-31 11:20:46 -0300

    service that lists all students and their courses

---
+++ src/services/getStudent.jsx
@@ -0,0 +1,8 @@
+//export const STUDENTS = import.meta.env.VITE_STUDENTS
+
+export const getStudents = async () =>{
+    const response = await fetch('/api/estudiantes');
+ //const response = await fetch(STUDENTS);
+    const result = await response.json();
+    return result;
+}
